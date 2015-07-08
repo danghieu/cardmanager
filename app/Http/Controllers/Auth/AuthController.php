4cb59c20bd03beac5dbc4c1f46af095be3a75919
hvthhien <hvthhien@gmail.com>
2015-07-08 17:54:17 +0700

    Sửa lại đăng nhập + đăng kí. P/S anh vô database thêm vào trước 1 dòng để lấy nó làm City mặc định cho các tài khoản. (id nó bằng 1).

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,38 +1,100 @@
 <?php namespace App\Http\Controllers\Auth;
 
+use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\Auth\Guard;
-use Illuminate\Contracts\Auth\Registrar;
-use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
 
 class AuthController extends Controller {
+	    /**
+     * the model instance
+     * @var User
+     */
+    protected $user; 
+    /**
+     * The Guard implementation.
+     *
+     * @var Authenticator
+     */
+    protected $auth;
 
-	/*
-	|--------------------------------------------------------------------------
-	| Registration & Login Controller
-	|--------------------------------------------------------------------------
-	|
-	| This controller handles the registration of new users, as well as the
-	| authentication of existing users. By default, this controller uses
-	| a simple trait to add these behaviors. Why don't you explore it?
-	|
-	*/
-
-	use AuthenticatesAndRegistersUsers;
-
-	/**
-	 * Create a new authentication controller instance.
-	 *
-	 * @param  \Illuminate\Contracts\Auth\Guard  $auth
-	 * @param  \Illuminate\Contracts\Auth\Registrar  $registrar
-	 * @return void
-	 */
-	public function __construct(Guard $auth, Registrar $registrar)
-	{
-		$this->auth = $auth;
-		$this->registrar = $registrar;
-
-		$this->middleware('guest', ['except' => 'getLogout']);
-	}
-
+    /**
+     * Create a new authentication controller instance.
+     *
+     * @param  Authenticator  $auth
+     * @return void
+     */
+    public function __construct(Guard $auth, User $user)
+    {
+        $this->user = $user; 
+        $this->auth = $auth;
+ 
+        $this->middleware('guest', ['except' => ['getLogout']]); 
+    }
+    /**
+     * Show the application login form.
+     *
+     * @return Response
+     */
+    public function getLogin()
+    {
+        return view('auth.login');
+    }
+ 
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  LoginRequest  $request
+     * @return Response
+     */
+    public function postLogin(LoginRequest $request)
+    {
+        if ($this->auth->attempt($request->only('email', 'password')))
+        {
+            return redirect('/home');
+        }
+ 
+        return redirect('/login')->withErrors([
+            'email' => 'Không tồn tại tài khoản với địa chỉ email này!',
+        ]);
+    }
+ 
+    /**
+     * Log the user out of the application.
+     *
+     * @return Response
+     */
+    public function getLogout()
+    {
+        $this->auth->logout();
+ 
+        return redirect('/');
+    }
+	 /**
+     * Show the application registration form.
+     *
+     * @return Response
+     */
+    public function getRegister()
+    {
+        return view('auth.register');
+    }
+ 
+    /**
+     * Handle a registration request for the application.
+     *
+     * @param  RegisterRequest  $request
+     * @return Response
+     */
+	public function postRegister(RegisterRequest $request)
+    {	
+		$this->user->name = $request->name;
+        $this->user->email = $request->email;
+        $this->user->password = bcrypt($request->password);
+        $this->user->place_manage = 1;
+        $this->user->save();
+        $this->auth->login($this->user);
+        return redirect('/home'); 
+    }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,11 @@
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
 			<div class="panel panel-default">
-				<div class="panel-heading">Login</div>
+				<div class="panel-heading">Đăng nhập</div>
 				<div class="panel-body">
 					@if (count($errors) > 0)
 						<div class="alert alert-danger">
-							<strong>Whoops!</strong> There were some problems with your input.<br><br>
+							<strong>Lỗi!</strong> Địa chỉ email hoặc mật khẩu không đúng.<br><br>
 							<ul>
 								@foreach ($errors->all() as $error)
 									<li>{{ $error }}</li>
@@ -22,14 +22,14 @@
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">E-Mail Address</label>
+							<label class="col-md-4 control-label">Địa chỉ E-mail</label>
 							<div class="col-md-6">
 								<input type="email" class="form-control" name="email" value="{{ old('email') }}">
 							</div>
 						</div>
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Password</label>
+							<label class="col-md-4 control-label">Mật khẩu</label>
 							<div class="col-md-6">
 								<input type="password" class="form-control" name="password">
 							</div>
@@ -39,7 +39,7 @@
 							<div class="col-md-6 col-md-offset-4">
 								<div class="checkbox">
 									<label>
-										<input type="checkbox" name="remember"> Remember Me
+										<input type="checkbox" name="remember"> Ghi nhớ đăng nhập
 									</label>
 								</div>
 							</div>
@@ -48,10 +48,10 @@
 						<div class="form-group">
 							<div class="col-md-6 col-md-offset-4">
 								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-									Login
+									Đăng nhập
 								</button>
 
-								<a href="/password/email">Forgot Your Password?</a>
+								<a href="/password/email">Quên mật khẩu?</a>
 							</div>
 						</div>
 					</form>
