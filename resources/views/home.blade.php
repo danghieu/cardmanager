eac87ad8ceef6ebec4bd5871b1b7b93d2a2f8b52
hvthhien <hvthhien@gmail.com>
2015-07-10 07:37:59 +0700

    giao diện trang chủ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,16 @@
 @section('content')
 <div class="container">
 	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
+		<div class="col-md-2">
+		<ul class="nav nav-pills nav-stacked">
+       
+        	<li><a class="cardmanager" href="#">Thông tin tài khoản</a></li>
+	        <li><a href="#">Quản Lý Thẻ</a></li>
+	        <li><a href="#">Lịch sử nạp - trừ tiền</a></li>
+	        <li><a href="#">Thông tin sai phạm</a></li>
+      </ul>
+		</div>
+		<div class="col-md-10">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
 
